270fe7563f432174363fa21f02b0e47eb3cd8433
Georg <mangoinsel@gmail.com>
2022-11-15 11:23:33 +0100

    Express 06  GET in detail

--- userhandlers.js
+++ userhandlers.js
@@ -22,9 +22,28 @@ const users = [
 const database = require("./database");
 
 const getUsers = (req, res) => {
+  let sql = "select * from users";
+
+  const sqlValues = [];
+
+  if (req.query.language != null) {
+    sql += " where language = ?";
+
+    sqlValues.push(req.query.language);
+
+    if (req.query.city != null) {
+      sql += "where city = ?";
+
+      sqlValues.push(req.query.city);
+    }
+  } else if (req.query.city != null) {
+    sql += " where city = ?";
+    sqlValues.push(req.query.city);
+  }
+
   database
 
-    .query("select * from users")
+    .query(sql, sqlValues)
 
     .then(([users]) => {
       res.json(users);
@@ -33,7 +52,7 @@ const getUsers = (req, res) => {
     .catch((err) => {
       console.error(err);
 
-      res.status(404).send("Not Found");
+      res.status(500).send("Error retrieving data from database");
     });
 };
 
